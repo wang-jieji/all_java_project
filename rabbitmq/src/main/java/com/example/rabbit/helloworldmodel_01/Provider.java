64cff53842563fa6bcdb4dd36da43d0b9606ebc1
WJJ <952144826@qq.com>
2021-04-06 02:47:31 +0800

    Ribbmq 模型

---
+++ rabbitmq/src/main/java/com/example/rabbit/helloworldmodel_01/Provider.java
@@ -0,0 +1,40 @@
+package com.example.rabbit.helloworldmodel_01;
+
+import com.example.rabbit.util.RabbitMQUtils;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.MessageProperties;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+/**
+ * @Description 第一种模型（直连） Provider(P) : 生产者，也就是要发送消息的程序  Customer(C): 消费者：消息的接受者，会一直等待消息到来
+ * http://192.168.206.133:15672 可以直接在web界面查看rabbitmq 默认账号 guest guest
+ * @Author WJJ
+ * @Date 2021/4/6 0:29
+ **/
+public class Provider {
+    public static void main(String[] args) throws IOException, TimeoutException {
+        Connection connection = RabbitMQUtils.getConnection();
+        // 获取连接中通道
+        Channel channel = connection.createChannel();
+        //通道绑定对应消息队列
+        // 参数1：队列名称 如果队列不存在自动创建
+        // 参数2：用来定义队列特性是否持久化   true 持久化队列（rabbitmq服务重启时将队列写入硬盘） false 不持久化（rabbitmq服务重启时删除队列）
+        // 参数3：exclusive 是否独占队列      true 独占队列（这个队列只接受这个连接的消息）  false 不独占
+        // 参数4：autoDelete: 是否在消息完成后自动删除队列  true 自动删除  false 不自动删除
+        // 参数5：额外附加参数
+        channel.queueDeclare("aa",true,false,true,null);
+
+        // 发布消息
+        // 参数1：交换机名称
+        // 参数2：队列名称
+        // 参数3：传递额外消息设置   MessageProperties.PERSISTENT_TEXT_PLAIN 消息持久化
+        // 参数4: 消息的具体内容
+        channel.basicPublish("","aa", MessageProperties.PERSISTENT_TEXT_PLAIN,"hello Customer".getBytes());
+
+        // 关闭通道
+        channel.close();
+        connection.close();
+    }
+}
---
+++ rabbitmq/src/main/java/com/example/rabbit/publishsubscribe_03/Provider.java
@@ -0,0 +1,40 @@
+package com.example.rabbit.publishsubscribe_03;
+
+import com.example.rabbit.util.RabbitMQUtils;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * @Description 第三种模型(fanout) 广播
+ * 在广播模式下，消息发送流程是这样的
+ * 1、可以有多个消费者
+ * 2、每个消费者有自己的queue（队列）
+ * 3、每个队列都要绑定到Exchange（交换机）
+ * 4、生产者发送的消息，只能发送到交换机，交换机来决定要发给哪个队列，生产者无法决定。
+ * 5、交换机把消息发送给绑定过的所有队列
+ * 6、队列的消费者都能拿到消息，实现一条消息被多个消费者消费
+ * @Author WJJ
+ * @Date 2021/4/6 0:29
+ **/
+public class Provider {
+    public static void main(String[] args) throws IOException {
+        Connection connection = RabbitMQUtils.getConnection();
+        // 获取连接中通道
+        Channel channel = connection.createChannel();
+
+        // 将通道声明指定交换机
+        // 参数1：交换机名称（随意命名）,如果交换机不存在，会自动创建
+        // 参数2：交换机类型  fanout 广播类型
+        channel.exchangeDeclare("logs","fanout");
+        // 参数1：交换机名称
+        // 参数2：队列名称
+        // 参数3：传递额外消息设置   MessageProperties.PERSISTENT_TEXT_PLAIN 消息持久化
+        // 参数4: 消息的具体内容
+        channel.basicPublish("logs","",null,"fanout type message".getBytes());
+        // 关闭连接
+        RabbitMQUtils.closeConnectionAndChanel(channel,connection);
+    }
+}
